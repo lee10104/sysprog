82c3240c67e8efeb83cfcc3439a8cb0e2720a4b7
lee10104 <lee95625@gmail.com>
2015-12-09 06:28:29 +0900

    생각해보니 시프 버렸는데 내가 왜 이거 해야 하지? 방학 때로 미룸

--- proxylab/proxy.c
+++ proxylab/proxy.c
@@ -1,8 +1,8 @@
 /*
  * proxy.c - M1522.000800 System Programming: Web proxy
  *
- * Student ID: 
- *         Name:
+ * Student ID:   2013-11422
+ *         Name: Lee Eunha
  * 
  * IMPORTANT: Give a high level description of your code here. You
  * must also provide a header comment at the beginning of each
@@ -26,18 +26,37 @@ ssize_t Rio_readn_w(int fd, void *ptr, size_t nbytes);
 ssize_t Rio_readlineb_w(rio_t *rp, void *usrbuf, size_t maxlen);
 void Rio_writen_w(int fd, void *usrbuf, size_t n);
 
+static FILE *flog;
+
 /*
  * main - Main routine for the proxy program
  */
 int main(int argc, char **argv)
 {
-    /* Check arguments */
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <port number>\n", argv[0]);
-        exit(0);
-    }
+  int listenfd, connfd, port;
+  socklen_t clientlen = sizeof(struct sockaddr_in);
+  struct sockaddr_in clientaddr;
 
+  /* Ignore SIGPIPE */
+  signal(SIGPIPE, SIG_IGN);
+
+  /* Check arguments */
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s <port number>\n", argv[0]);
     exit(0);
+  }
+
+  port = atoi(argv[1]);
+
+  flog = Fopen(PROXY_LOG, "a");
+  listenfd = Open_listenfd(port);
+
+  while (1)
+  {
+    connfd = Accept(listenfd, (SA *) &clientaddr, &clientlen);
+  }
+
+  exit(0);
 }
 
 
